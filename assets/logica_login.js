f757a85bcf9d83cec495c0a4add1ab4c28df0958
samuel216progra <srodriguez-2022165@kinal.edu.gt>
2024-02-02 11:46:56 -0600

    se suben las vistas individuales

--- assets/logica_login.js
+++
@@ -1,9 +0,0 @@
-function go() {
-
-    if (document.form.password.value == 'password' && document.form.login.value == 'user') {
-        document.form.submit();
-    }
-    else {
-        alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
-    }
-} 
